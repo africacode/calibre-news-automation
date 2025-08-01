43673a052a16cfbd10179d7912c35c9f5fd038d4
africacode <africaparientefora@gmail.com>
2025-08-01 13:23:10 +0200

    Create descargar_y_enviar.py

---
+++ descargar_y_enviar.py
@@ -0,0 +1,35 @@
+import os
+import time
+import subprocess
+from email.message import EmailMessage
+import smtplib
+
+recipes = {
+    'El_Mundo': 'recipes/elmundo.recipe',
+    'Expansion': 'recipes/expansion_spanish.recipe',
+    'Wall_Street_Journal': 'recipes/wsj_news.recipe'
+}
+
+FROM_EMAIL = os.getenv('FROM_EMAIL')
+TO_EMAIL = os.getenv('TO_EMAIL')
+APP_PASSWORD = os.getenv('APP_PASSWORD')
+
+epubs = []
+for name, path in recipes.items():
+    epub = f"{name}.epub"
+    subprocess.run(['ebook-convert', path, epub], check=True)
+    epubs.append(epub)
+    time.sleep(5)
+
+msg = EmailMessage()
+msg['From'] = FROM_EMAIL
+msg['To'] = TO_EMAIL
+msg['Subject'] = "Tus periódicos del día"
+
+for epub in epubs:
+    with open(epub, 'rb') as f:
+        msg.add_attachment(f.read(), maintype='application', subtype='epub+zip', filename=epub)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(FROM_EMAIL, APP_PASSWORD)
+    smtp.send_message(msg)
